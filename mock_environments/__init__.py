20aa146d3c8ebfdc53eb367893c2ae3c30bbb291
uncovsky <vit.uncovsky@gmail.com>
2025-08-16 21:56:27 +0200

    fix module in env init

--- mock_environments/__init__.py
+++ mock_environments/__init__.py
@@ -6,7 +6,7 @@ from gymnasium.envs.registration import register
 
 register(
     id="SquareReachEnv-v0",
-    entry_point="square_reach:SquareReachEnv",
+    entry_point="mock_environments.square_reach:SquareReachEnv",
     max_episode_steps=1000,  # default, can be overridden
 )
 
