87e6de534a4b697396afb90c4355a4e63a38146e
uncovsky <vit.uncovsky@gmail.com>
2025-09-07 15:51:24 +0200

    Diversity run exps

--- experimental/experiments/diversity_experiments/run_experiment.py
+++ experimental/experiments/diversity_experiments/run_experiment.py
@@ -5,19 +5,25 @@ import os
 os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
 
 folder = "experiments/diversity_experiments/configs"
+folder_priors = "experiments/diversity_experiments/configs/priors"
 
-def run_experiment(entity, project):
-    sweep_folder(folder, entity, project)
+def run_experiment(entity, project, prior=False):
+    if prior:
+        sweep_folder(folder_priors, entity, project)
+    else:
+    s   weep_folder(folder, entity, project)
 
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description="Run square reach experiment.")
     parser.add_argument("--entity", type=str, required=True, help="WandB entity name")
+    parser.add_argument("--prior", type=bool, default=False, help="Whether to use randomized priors")
     parser.add_argument("--project", type=str, required=True, help="WandB project name")
 
+    # help
     args = parser.parse_args()
-    run_experiment(args.entity, args.project)
+    run_experiment(args.entity, args.project, args.prior)
 
 
 
