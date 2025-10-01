a8f4a4ce047686af9906904535f8587b8853cf50
xuncovsk <xuncovsk@fi.muni.cz>
2025-10-01 17:42:33 +0200

    Fix collecting data

--- experimental/experiments/collect_data.py
+++ experimental/experiments/collect_data.py
@@ -1,8 +1,10 @@
 from experiments import collect_bandit_data
 
 def collect_data():
-    # Collect data for continuous bandit
     collect_bandit_data(episodes=1000, seed=0)
+
+if __name__ == "__main__":
+    collect_data()
 
 
 
