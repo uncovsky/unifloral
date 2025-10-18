f2d71a3350cc7ccf4d681b86a6f62157bf8201cd
uncovsky <vit.uncovsky@gmail.com>
2025-10-18 18:57:42 +0200

    fix imports

--- experimental/infra/__init__.py
+++ experimental/infra/__init__.py
@@ -1,7 +1,7 @@
 from infra.ensemble_training import select_regularizer, make_pretrain_step, select_ood_regularizer
 from infra.dataset import OfflineDatasetWrapper
 from infra.utils import *
-from infra.models import TanhGaussianActor, EntropyCoef, PriorVectorQ, VectorQ, NormalizationWrapper
+from infra.models import TanhGaussianActor, EntropyCoef, PriorVectorQ, VectorQ
 
 __all__ = [
     "OfflineDatasetWrapper",
@@ -9,7 +9,6 @@ __all__ = [
     "EntropyCoef",
     "PriorVectorQ",
     "VectorQ",
-    "NormalizationWrapper",
     "select_regularizer",
     "select_ood_regularizer",
     "make_pretrain_step",
