b1a0219d5b6e5450dcb6b211032e48c59938f5db
uncovsky <vit.uncovsky@gmail.com>
2025-10-21 18:09:18 +0200

    different dims

--- experimental/experiments/collect_data.py
+++ experimental/experiments/collect_data.py
@@ -1,7 +1,7 @@
 from experiments import collect_bandit_data, collect_cql_data, collect_uniform_data, collect_d_dim_data
 
 def collect_data():
-    collect_d_dim_data(episodes=1000, ds=[2,5,10], seeds=[0,1,2])
+    collect_d_dim_data(episodes=1000, ds=[2,4, 6], seeds=[0,1,2])
     collect_uniform_data(episodes=1000, seed=0)
     collect_cql_data(episodes=10000, seed=0)
     collect_bandit_data(episodes=1000, seed=0)
